596aa7aa837c8d22b8625f55ac079a1044cb1702
qiaelel <qiaelel@gmail.com>
2025-10-19 12:21:49 +0300

    update

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import config
 import _sqlite3
-from telebot import TeleBot
+from telebot import TeleBot, callback_data, types
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
-from telebot import types
+
 
 token = config.token
 admin = config.admin
 
-print(admin)
 bot = TeleBot(token, parse_mode='MARKDOWN')
+print("Здесь будут отображаться логи:")
 
 #кнопка возврата в главное меню
 backinline = types.InlineKeyboardMarkup(row_width=1)
 backinline_types = types.InlineKeyboardButton(text='Вернуться в главное меню', callback_data='back')
 backinline.add(backinline_types)
 
+#главная страница
 @bot.message_handler(commands=['start'])
 def welcome(message):
     user_id = message.from_user.id
@@ -25,6 +26,24 @@ def welcome(message):
     kb.add(schedule, help_author, profile)
     bot.send_message(message.chat.id, "Привет! Ты находишься в главном меню бота для онлайн школы. ", reply_markup=kb, parse_mode='HTML')
 
+#профиль
+@bot.callback_query_handler(func=lambda call: True)
+def profile(message):
+    if callback_data == 'profile':
+        user_id = message.from_user.id
+        bot.answer_callback_query(call.id, 'Профиль')
+        bot.send_message(message.chat.id, f'Ваш id: {user_id}')
+    if callback_data == 'back':
+         user_id = message.from_user.id
+    kb = types.InlineKeyboardMarkup(row_width=1)
+    schedule = types.InlineKeyboardButton(text='Просмотреть расписание', callback_data='schedule')
+    help_author = types.InlineKeyboardButton(text='Помощь', callback_data='help_author')
+    profile = types.InlineKeyboardButton(text="Профиль", callback_data='profile')
+    kb.add(schedule, help_author, profile)
+    bot.send_message(message.chat.id, "Привет! Ты находишься в главном меню бота для онлайн школы. ", reply_markup=kb, parse_mode='HTML')
+    
+
+#админпанель
 @bot.message_handler(commands=['admin'])
 def adminpanel(message):
     user_id = message.from_user.id
@@ -37,7 +56,10 @@ def adminpanel(message):
         bot.send_message(message.chat.id, "Добро пожаловать в админ панель!", reply_markup=kb, parse_mode='HTML')
     else:
         print("Пользователь", user_id, "попытался получить доступ к админпанели.")
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        kb.add(backinline_types)
         bot.send_message(message.chat.id, "Отказано в доступе.", reply_markup=backinline, parse_mode='HTML')
+
         
 bot.infinity_polling()
 
